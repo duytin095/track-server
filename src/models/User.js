bc9cde22a211f14ae9d8a12dc7fe4a434b503de7
Duy Tín <duytin095@gmail.com>
2024-04-26 17:49:31 +0700

    salting and hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const useSchema = new mongoose.Schema({
+const bcrypt =  require('bcrypt');
+const userSchema = new mongoose.Schema({
     email: {
         type: String,
         unique: true,
@@ -11,4 +12,39 @@ const useSchema = new mongoose.Schema({
     }
 });
 
-mongoose.model('User', useSchema);
+userSchema.pre('save', function(){
+    const user = this;
+    if(!user.isModified('password')){
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) =>{
+        if(err){
+            return next(err);
+        }
+        bcrypt.hash(user.password, salt, (err, bcrypt) =>{
+            if(err){
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function (candicatePassword){
+    const user = this;
+return new Promise((resolve, reject) =>{
+bcrypt.compare(candicatePassword, user.password, (err, isMatch) => {
+    if(err){
+        return reject(err);
+    }
+    if(!isMatch){
+        return reject(false);
+    }
+    resolve(true);
+})
+});
+}
+
+mongoose.model('User', userSchema);
